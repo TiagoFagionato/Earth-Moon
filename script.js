2f6af103ad4c4a3825b14d8bc06d3a8bc7e55c92
Tiago Fagionato <tiagofagionato@gmail.com>
2018-12-28 01:54:18 -0200

    functions,space,Earth

--- script.js
+++ script.js
@@ -5,7 +5,39 @@ window.onload = function() {
 function main() {
 	var canvas = document.getElementById('canvas');
 	ctx = canvas.getContext('2d');
-	ctx.fillRect(25, 25, 100, 100);
-	ctx.clearRect(45, 45, 60, 60);
-	ctx.strokeRect(50, 50, 50, 50);
+
+	function line() {
+		for(var x = 0; x <= 480; x = x + 40) {
+			ctx.beginPath();
+			ctx.moveTo(x, 0);
+			ctx.lineTo(x, 480);
+			ctx.moveTo(0, x);
+			ctx.lineTo(480, x);
+			ctx.strokeStyle = "#2B2B2B";
+			ctx.lineWidth = 0;
+			ctx.stroke();
+		}
+	}
+
+	function draw() {
+		//Earth
+		ctx.beginPath();
+		ctx.arc(240, 240, 60, -(Math.PI / 180) * (90), (Math.PI / 180) * (90), false);
+		ctx.lineWidth = 0;
+		ctx.fillStyle = "#090968";
+		ctx.fill();
+		ctx.beginPath();
+		ctx.arc(240, 240, 60, (Math.PI / 180) * (90), -(Math.PI / 180) * (90), false);
+		ctx.lineWidth = 0;
+		ctx.fillStyle = "#121212";
+		ctx.fill();
+	}
+
+	function loop() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		//line();
+		draw();
+	}
+	loop();
+	setInterval(loop, 1000);
 }
